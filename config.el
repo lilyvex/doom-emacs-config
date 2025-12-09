9cc5b983d41ad99b1c1c8d8d48ffedfca98cdb4c
lily <lesson085@gmail.com>
2025-12-09 18:44:57 -0500

    feat: initial commit

---
+++ config.el
@@ -0,0 +1,118 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets. It is optional.
+;; (setq user-full-name "John Doe"
+;;       user-mail-address "john@doe.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
+;;
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-symbol-font' -- for symbols
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+;;
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+(setq doom-font (font-spec :family "CaskaydiaCove Nerd Font Mono" :size 12 :weight 'semi-light)
+     doom-variable-pitch-font (font-spec :family "CaskaydiaCove Nerd Font Propo" :size 13))
+(custom-set-faces
+ '(font-lock-comment-face ((t (:family "CaskaydiaCove Nerd Font Propo" :height 1.0 :weight normal :slant italic)))))
+(doom-big-font-mode 1)
+
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-feather-light)
+(add-to-list 'default-frame-alist '(alpha-background . 80))
+(setq doom-modeline-height 35)
+(setq doom-modeline-icon t)
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type 'relative)
+
+;; Disable Vi tildes after EOF
+(remove-hook 'text-mode-hook #'vi-tilde-fringe-mode)
+(remove-hook 'prog-mode-hook #'vi-tilde-fringe-mode)
+
+;; Enable Discord RPC
+(elcord-mode 1)
+
+;; Configure spdx.el
+(map! :leader :desc "Insert SPDX-License-Identifier" "i l" #'spdx-insert-spdx)
+
+(use-package! spdx
+  :config
+  (setq spdx-copyright-holder "Lily Vex"
+        spdx-project-detection 'auto))
+
+;; Copy to system clipboard
+(map! "C-c C-c" #'clipboard-kill-ring-save)
+
+;; Set vterm shell
+(setq vterm-shell "/usr/bin/zsh")
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+;; AstroJS LSP-Mode with optional TailwindCSS and Typescript language server support
+(use-package astro-mode
+  :ensure nil
+  :mode "\\.astro\\'"
+  :config
+  (add-hook 'astro-mode-hook #'lsp-deferred t))
+
+(define-derived-mode astro-mode web-mode "Astro"
+  "Major mode for editing Astro files."
+  (message "Astro mode is enabled for this buffer."))
+
+(after! nerd-icons
+  (add-to-list 'nerd-icons-extension-icon-alist
+               '("astro" nerd-icons-devicon "nf-dev-astro" :face nerd-icons-orange)))
+
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
