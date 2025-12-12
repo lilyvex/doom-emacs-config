7ac4c9f3a720a8d6223ddbb2418d2d914da5e90c
lily <lesson085@gmail.com>
2025-12-12 14:37:19 -0500

    fix: ensure font only changes in graphical mode

--- config.el
+++ config.el
@@ -21,11 +21,17 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-(setq doom-font (font-spec :family "CaskaydiaCove Nerd Font Mono" :size 12 :weight 'semi-light)
-     doom-variable-pitch-font (font-spec :family "CaskaydiaCove Nerd Font Propo" :size 13))
-(custom-set-faces
- '(font-lock-comment-face ((t (:family "CaskaydiaCove Nerd Font Propo" :height 1.0 :weight normal :slant italic)))))
-(doom-big-font-mode 1)
+
+(if (display-graphic-p)
+    (setq doom-font (font-spec :family "CaskaydiaCove Nerd Font Mono" :size 12 :weight 'semi-light)
+          doom-variable-pitch-font (font-spec :family "CaskaydiaCove Nerd Font Propo" :size 13)))
+
+(if (display-graphic-p)
+    (custom-set-faces
+     '(font-lock-comment-face ((t (:family "CaskaydiaCove Nerd Font Propo" :height 1.0 :weight normal :slant italic))))))
+
+(if (display-graphic-p)
+    (doom-big-font-mode 1))
 
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
